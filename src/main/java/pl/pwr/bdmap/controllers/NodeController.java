b84796a75ae4edf691fe8805860ae6d2922caa09
durki <michal@durkalec.com>
2023-12-10 20:20:25 +0100

    Implemented DTO for Node. Working "/nodes" endpoint

--- src/main/java/pl/pwr/bdmap/controllers/NodeController.java
+++ src/main/java/pl/pwr/bdmap/controllers/NodeController.java
@@ -3,21 +3,30 @@ package pl.pwr.bdmap.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.pwr.bdmap.dto.NodeDTO;
+import pl.pwr.bdmap.dto.NodeDTOMapper;
 import pl.pwr.bdmap.model.Node;
 import pl.pwr.bdmap.repositories.NodeRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class NodeController {
     private final NodeRepository nodeRepository;
+    private final NodeDTOMapper nodeDTOMapper;
 
-    public NodeController(NodeRepository nodeRepository) {
+    public NodeController(NodeRepository nodeRepository, NodeDTOMapper nodeDTOMapper) {
         this.nodeRepository = nodeRepository;
+        this.nodeDTOMapper = nodeDTOMapper;
     }
+
     @GetMapping("/nodes")
-    List<Node> all() {
-        return (List<Node>) nodeRepository.findAll();
+    List<NodeDTO> all() {
+        List<Node> list = new ArrayList<Node>();
+        nodeRepository.findAll().forEach(list::add);
+        return list.stream().map(nodeDTOMapper).collect(Collectors.toList());
     }
 
     @GetMapping("/nodes/range")
