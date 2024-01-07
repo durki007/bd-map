2fea34367e6dd084c200a9e9c8cdb5e1a59fb62e
durki <michal@durkalec.com>
2024-01-07 17:16:20 +0100

    Created DAO for WayNode

---
+++ src/main/java/pl/pwr/bdmap/dao/WayNodeRepository.java
@@ -0,0 +1,13 @@
+package pl.pwr.bdmap.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import pl.pwr.bdmap.model.WayNode;
+
+import java.util.List;
+
+public interface WayNodeRepository extends CrudRepository<WayNode, Integer> {
+
+    List<WayNode> findAllByWay_Id(int id);
+    List<WayNode> findAllByNode1_Id(int id);
+    List<WayNode> findAllByNode2_Id(int id);
+}
