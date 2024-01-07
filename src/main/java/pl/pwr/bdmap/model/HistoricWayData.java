7566ccb549384d4b7c2215ff6a8b3c86127c97d1
durki <michal@durkalec.com>
2024-01-07 22:13:05 +0100

    Cleanup models

--- src/main/java/pl/pwr/bdmap/model/HistoricWayData.java
+++ src/main/java/pl/pwr/bdmap/model/HistoricWayData.java
@@ -1,107 +1,104 @@
 package pl.pwr.bdmap.model;
+
 import java.io.Serializable;
+
 import jakarta.persistence.*;
 
 @Entity
-@org.hibernate.annotations.Proxy(lazy=false)
-@Table(name="HistoricWayData")
+@Table(name = "HistoricWayData")
 public class HistoricWayData implements Serializable {
-	public HistoricWayData() {
-	}
-	
-	@Column(name="id", nullable=false, unique=true, length=10)
-	@Id
-	@GeneratedValue(generator="HISTORICWAYDATA_ID_GENERATOR")	
-	@org.hibernate.annotations.GenericGenerator(name="HISTORICWAYDATA_ID_GENERATOR", strategy="native")	
-	private int id;
-	
-	@Column(name="name", nullable=false, length=255)	
-	private String name;
+    public HistoricWayData() {
+        // Hibernate Empty constructor
+    }
 
-	@Column(name="timestamp", nullable=false)	
-	private java.sql.Timestamp timestamp;
-	
-	@Column(name="version", nullable=false, length=10)	
-	private int version;
-	
-	@ManyToOne(targetEntity= Changeset.class, fetch=FetchType.LAZY)
-	@org.hibernate.annotations.Cascade({org.hibernate.annotations.CascadeType.LOCK})	
-	@JoinColumns(value={ @JoinColumn(name="changeset_id", referencedColumnName="id", nullable=false) }, foreignKey=@ForeignKey(name="FKHistoricWa184112"))	
-	private Changeset changeset;
-	
-	@ManyToOne(targetEntity= Way.class, fetch=FetchType.LAZY)
-	@org.hibernate.annotations.Cascade({org.hibernate.annotations.CascadeType.LOCK})	
-	@JoinColumns(value={ @JoinColumn(name="way_id", referencedColumnName="id", nullable=false) }, foreignKey=@ForeignKey(name="FKHistoricWa730677"))	
-	private Way way;
-	
-	@OneToMany(mappedBy="historicWayData", targetEntity= HistoricWayNode.class)
-	@org.hibernate.annotations.Cascade({org.hibernate.annotations.CascadeType.SAVE_UPDATE, org.hibernate.annotations.CascadeType.LOCK})	
-	@org.hibernate.annotations.LazyCollection(org.hibernate.annotations.LazyCollectionOption.TRUE)	
-	private java.util.Set historicWayNode = new java.util.HashSet();
-	
-	private void setId(int value) {
-		this.id = value;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	
-	public int getORMID() {
-		return getId();
-	}
-	
-	public void setName(String value) {
-		this.name = value;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setTimestamp(java.sql.Timestamp value) {
-		this.timestamp = value;
-	}
-	
-	public java.sql.Timestamp getTimestamp() {
-		return timestamp;
-	}
-	
-	public void setVersion(int value) {
-		this.version = value;
-	}
-	
-	public int getVersion() {
-		return version;
-	}
-	
-	public void setChangeset(Changeset value) {
-		this.changeset = value;
-	}
-	
-	public Changeset getChangeset() {
-		return changeset;
-	}
-	
-	public void setWay(Way value) {
-		this.way = value;
-	}
-	
-	public Way getWay() {
-		return way;
-	}
-	
-	public void setHistoricWayNode(java.util.Set value) {
-		this.historicWayNode = value;
-	}
-	
-	public java.util.Set getHistoricWayNode() {
-		return historicWayNode;
-	}
-	
-	
-	public String toString() {
-		return String.valueOf(getId());
-	}
-	
+    @Column(name = "id", nullable = false, unique = true, length = 10)
+    @Id
+    @GeneratedValue(generator = "HISTORICWAYDATA_ID_GENERATOR", strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "name", nullable = false, length = 255)
+    private String name;
+
+    @Column(name = "timestamp", nullable = false)
+    private java.sql.Timestamp timestamp;
+
+    @Column(name = "version", nullable = false, length = 10)
+    private int version;
+
+    @ManyToOne(targetEntity = Changeset.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumns(value = {@JoinColumn(name = "changeset_id", referencedColumnName = "id", nullable = false)}, foreignKey = @ForeignKey(name = "FKHistoricWa184112"))
+    private Changeset changeset;
+
+    @ManyToOne(targetEntity = Way.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumns(value = {@JoinColumn(name = "way_id", referencedColumnName = "id", nullable = false)}, foreignKey = @ForeignKey(name = "FKHistoricWa730677"))
+    private Way way;
+
+    @OneToMany(mappedBy = "historicWayData", targetEntity = HistoricWayNode.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private java.util.Set<HistoricWayNode> historicWayNode = new java.util.HashSet<>();
+
+    private void setId(int value) {
+        this.id = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getORMID() {
+        return getId();
+    }
+
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setTimestamp(java.sql.Timestamp value) {
+        this.timestamp = value;
+    }
+
+    public java.sql.Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setVersion(int value) {
+        this.version = value;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setChangeset(Changeset value) {
+        this.changeset = value;
+    }
+
+    public Changeset getChangeset() {
+        return changeset;
+    }
+
+    public void setWay(Way value) {
+        this.way = value;
+    }
+
+    public Way getWay() {
+        return way;
+    }
+
+    public void setHistoricWayNode(java.util.Set<HistoricWayNode> value) {
+        this.historicWayNode = value;
+    }
+
+    public java.util.Set<HistoricWayNode> getHistoricWayNode() {
+        return historicWayNode;
+    }
+
+
+    public String toString() {
+        return String.valueOf(getId());
+    }
+
 }
--- src/main/java/pl/pwr/bdmap/model/HistoricWayNode.java
+++ src/main/java/pl/pwr/bdmap/model/HistoricWayNode.java
@@ -1,85 +1,82 @@
 package pl.pwr.bdmap.model;
 
 import java.io.Serializable;
+
 import jakarta.persistence.*;
 
 @Entity
-@org.hibernate.annotations.Proxy(lazy=false)
-@Table(name="HistoricWayNode")
+@org.hibernate.annotations.Proxy(lazy = false)
+@Table(name = "HistoricWayNode")
 public class HistoricWayNode implements Serializable {
-	public HistoricWayNode() {
-	}
-	
-	@Column(name="id", nullable=false, unique=true, length=10)	
-	@Id	
-	@GeneratedValue(generator="HISTORICWAYNODE_ID_GENERATOR")	
-	@org.hibernate.annotations.GenericGenerator(name="HISTORICWAYNODE_ID_GENERATOR", strategy="native")	
-	private int id;
-	
-	@Column(name="version", nullable=false, length=10)	
-	private int version;
-	
-	@ManyToOne(targetEntity=HistoricNodeData.class, fetch=FetchType.LAZY)	
-	@org.hibernate.annotations.Cascade({org.hibernate.annotations.CascadeType.LOCK})	
-	@JoinColumns(value={ @JoinColumn(name="node1_id", referencedColumnName="id", nullable=false) }, foreignKey=@ForeignKey(name="FKHistoricWa603500"))	
-	private HistoricNodeData node1;
-	
-	@ManyToOne(targetEntity=HistoricNodeData.class, fetch=FetchType.LAZY)	
-	@org.hibernate.annotations.Cascade({org.hibernate.annotations.CascadeType.LOCK})	
-	@JoinColumns(value={ @JoinColumn(name="node2_id", referencedColumnName="id", nullable=false) }, foreignKey=@ForeignKey(name="FKHistoricWa633291"))	
-	private HistoricNodeData node2;
-	
-	@ManyToOne(targetEntity=HistoricWayData.class, fetch=FetchType.LAZY)	
-	@org.hibernate.annotations.Cascade({org.hibernate.annotations.CascadeType.LOCK})	
-	@JoinColumns(value={ @JoinColumn(name="HistoricWayData_Id", referencedColumnName="id", nullable=false) }, foreignKey=@ForeignKey(name="FKHistoricWa481251"))	
-	private HistoricWayData historicWayData;
-	
-	private void setId(int value) {
-		this.id = value;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	
-	public int getORMID() {
-		return getId();
-	}
-	
-	public void setVersion(int value) {
-		this.version = value;
-	}
-	
-	public int getVersion() {
-		return version;
-	}
-	
-	public void setNode1(HistoricNodeData value) {
-		this.node1 = value;
-	}
-	
-	public HistoricNodeData getNode1() {
-		return node1;
-	}
-	
-	public void setNode2(HistoricNodeData value) {
-		this.node2 = value;
-	}
-	
-	public HistoricNodeData getNode2() {
-		return node2;
-	}
-	
-	public void setHistoricWayData(HistoricWayData value) {
-		this.historicWayData = value;
-	}
-	
-	public HistoricWayData getHistoricWayData() {
-		return historicWayData;
-	}
-	
-	public String toString() {
-		return String.valueOf(getId());
-	}
-	
+    public HistoricWayNode() {
+    }
+
+    @Column(name = "id", nullable = false, unique = true, length = 10)
+    @Id
+    @GeneratedValue(generator = "HISTORICWAYNODE_ID_GENERATOR", strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "version", nullable = false, length = 10)
+    private int version;
+
+    @ManyToOne(targetEntity = HistoricNodeData.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumns(value = {@JoinColumn(name = "node1_id", referencedColumnName = "id", nullable = false)}, foreignKey = @ForeignKey(name = "FKHistoricWa603500"))
+    private HistoricNodeData node1;
+
+    @ManyToOne(targetEntity = HistoricNodeData.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumns(value = {@JoinColumn(name = "node2_id", referencedColumnName = "id", nullable = false)}, foreignKey = @ForeignKey(name = "FKHistoricWa633291"))
+    private HistoricNodeData node2;
+
+    @ManyToOne(targetEntity = HistoricWayData.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumns(value = {@JoinColumn(name = "HistoricWayData_Id", referencedColumnName = "id", nullable = false)}, foreignKey = @ForeignKey(name = "FKHistoricWa481251"))
+    private HistoricWayData historicWayData;
+
+    private void setId(int value) {
+        this.id = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getORMID() {
+        return getId();
+    }
+
+    public void setVersion(int value) {
+        this.version = value;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setNode1(HistoricNodeData value) {
+        this.node1 = value;
+    }
+
+    public HistoricNodeData getNode1() {
+        return node1;
+    }
+
+    public void setNode2(HistoricNodeData value) {
+        this.node2 = value;
+    }
+
+    public HistoricNodeData getNode2() {
+        return node2;
+    }
+
+    public void setHistoricWayData(HistoricWayData value) {
+        this.historicWayData = value;
+    }
+
+    public HistoricWayData getHistoricWayData() {
+        return historicWayData;
+    }
+
+    public String toString() {
+        return String.valueOf(getId());
+    }
+
 }
