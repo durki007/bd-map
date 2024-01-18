334e6a4600e32d4d0e778eb80d48267b84e7bd4d
xdx1354 <stanislaw1354@gmail.com>
2024-01-18 15:45:45 +0100

    Node list catching exceptions

--- src/main/java/pl/pwr/bdmap/dao/NodeRepository.java
+++ src/main/java/pl/pwr/bdmap/dao/NodeRepository.java
@@ -1,5 +1,6 @@
 package pl.pwr.bdmap.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import pl.pwr.bdmap.model.Node;
 import pl.pwr.bdmap.model.NodeType;
