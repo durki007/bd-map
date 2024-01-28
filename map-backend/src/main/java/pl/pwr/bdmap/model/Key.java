d14511af2406cb1a80aa5641a3d1ebe5fdbffa1e
durki <michal@durkalec.com>
2024-01-28 20:49:48 +0100

    Updated model to support key value pairs

--- map-backend/src/main/java/pl/pwr/bdmap/model/Key.java
+++ map-backend/src/main/java/pl/pwr/bdmap/model/Key.java
@@ -1,92 +1,105 @@
 package pl.pwr.bdmap.model;
 
 import java.io.Serializable;
+
 import jakarta.persistence.*;
+
 @Entity
-@Table(name="`Key`")
+@Table(name = "`Key`")
 public class Key implements Serializable {
-	public Key() {
-		// Empty constructor
-	}
-	
-	@Column(name="id", nullable=false, unique=true, length=10)	
-	@Id	
-	@GeneratedValue(generator="KEY_ID_GENERATOR", strategy = GenerationType.AUTO)
-	private int id;
-	
-	@Column(name="value", nullable=false, length=255, unique = true)
-	private String value;
-	
-	@OneToMany(mappedBy="key", targetEntity= KeyNodeType.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	private java.util.Set<KeyNodeType> keyNodeType = new java.util.HashSet<>();
-	
-	@OneToMany(mappedBy="key", targetEntity= KeyWayType.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	private java.util.Set<KeyWayType> keyWayType = new java.util.HashSet<>();
-	
-	@OneToMany(mappedBy="key", targetEntity= WayTag.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	private java.util.Set<WayTag> wayTag = new java.util.HashSet<>();
-	
-	@OneToMany(mappedBy="key", targetEntity= NodeTag.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	private java.util.Set<NodeTag> nodeTag = new java.util.HashSet<>();
-	
-	private void setId(int value) {
-		this.id = value;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	
-	public int getORMID() {
-		return getId();
-	}
-	
-	public void setValue(String value) {
-		this.value = value;
-	}
-	
-	public String getValue() {
-		return value;
-	}
-	
-	public void setKeyNodeType(java.util.Set<KeyNodeType> value) {
-		this.keyNodeType = value;
-	}
-	
-	public java.util.Set<KeyNodeType> getKeyNodeType() {
-		return keyNodeType;
-	}
-	
-	
-	public void setKeyWayType(java.util.Set<KeyWayType> value) {
-		this.keyWayType = value;
-	}
-	
-	public java.util.Set<KeyWayType> getKeyWayType() {
-		return keyWayType;
-	}
-	
-	
-	public void setWayTag(java.util.Set<WayTag> value) {
-		this.wayTag = value;
-	}
-	
-	public java.util.Set<WayTag> getWayTag() {
-		return wayTag;
-	}
-	
-	
-	public void setNodeTag(java.util.Set<NodeTag> value) {
-		this.nodeTag = value;
-	}
-	
-	public java.util.Set<NodeTag> getNodeTag() {
-		return nodeTag;
-	}
-	
-	
-	public String toString() {
-		return String.valueOf(getId());
-	}
-	
+    public Key() {
+        // Empty constructor
+    }
+
+    @Column(name = "id", nullable = false, unique = true, length = 10)
+    @Id
+    @GeneratedValue(generator = "KEY_ID_GENERATOR", strategy = GenerationType.AUTO)
+    private int id;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    @Column(name = "key", nullable = false, length = 255, unique = false)
+    private String key;
+
+    @Column(name = "value", nullable = false, length = 255, unique = false)
+    private String value;
+
+    @OneToMany(mappedBy = "key", targetEntity = KeyNodeType.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private java.util.Set<KeyNodeType> keyNodeType = new java.util.HashSet<>();
+
+    @OneToMany(mappedBy = "key", targetEntity = KeyWayType.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private java.util.Set<KeyWayType> keyWayType = new java.util.HashSet<>();
+
+    @OneToMany(mappedBy = "key", targetEntity = WayTag.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private java.util.Set<WayTag> wayTag = new java.util.HashSet<>();
+
+    @OneToMany(mappedBy = "key", targetEntity = NodeTag.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private java.util.Set<NodeTag> nodeTag = new java.util.HashSet<>();
+
+    private void setId(int value) {
+        this.id = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getORMID() {
+        return getId();
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setKeyNodeType(java.util.Set<KeyNodeType> value) {
+        this.keyNodeType = value;
+    }
+
+    public java.util.Set<KeyNodeType> getKeyNodeType() {
+        return keyNodeType;
+    }
+
+
+    public void setKeyWayType(java.util.Set<KeyWayType> value) {
+        this.keyWayType = value;
+    }
+
+    public java.util.Set<KeyWayType> getKeyWayType() {
+        return keyWayType;
+    }
+
+
+    public void setWayTag(java.util.Set<WayTag> value) {
+        this.wayTag = value;
+    }
+
+    public java.util.Set<WayTag> getWayTag() {
+        return wayTag;
+    }
+
+
+    public void setNodeTag(java.util.Set<NodeTag> value) {
+        this.nodeTag = value;
+    }
+
+    public java.util.Set<NodeTag> getNodeTag() {
+        return nodeTag;
+    }
+
+
+    public String toString() {
+        return String.valueOf(getId());
+    }
+
 }
