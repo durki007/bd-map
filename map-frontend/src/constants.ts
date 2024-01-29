87f1febef2c5ab385380b5c4c678d1db6149c9de
Michał Korczak <michal.k2001@gmail.com>
2024-01-29 15:17:18 +0100

    dziala (#57)
    * fix: api url
    
    * fix: close changeset
    
    * feat: final fix

--- map-frontend/src/constants.ts
+++ map-frontend/src/constants.ts
@@ -1,3 +1,3 @@
-const host = process.env.API_HOST
-const port = process.env.API_PORT
+const host = process.env.NEXT_PUBLIC_API_HOST
+const port = process.env.NEXT_PUBLIC_API_PORT
 export const API_URL = `http://${host}:${port}`;
